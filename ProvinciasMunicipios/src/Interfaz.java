84076fb8c9aa198c640cb4b09a63a85e90e73d1b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-05-01 08:50:02 +0200

    Añadir un municipio a la provincia seleccionada. Actualizar lista de municipios

--- ProvinciasMunicipios/src/Interfaz.java
+++ ProvinciasMunicipios/src/Interfaz.java
@@ -36,6 +36,37 @@ public class Interfaz extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, ex.getMessage());
         }
     }
+    
+    private boolean insertarCategoria(String _nombre, int _id_provincia) throws SQLException {
+        Statement sentencia = conexion.createStatement();
+        String sql = "INSERT INTO municipios(`id_provincia`, `cod_municipio`, `DC`, `nombre`)"
+                + "VALUES (" + _id_provincia + ", 1, 1, '" + _nombre + "')";
+        return (sentencia.executeUpdate(sql) > 0);
+
+    }
+    
+    private void actualizarListaMunicipios() {
+        lm.clear();
+        String provin = String.valueOf(provincias.getSelectedItem());
+
+        try {  //   Mostramos los municipios de la provincia seleccionada
+            Statement s = conexion.createStatement();
+            if (provincias.getSelectedIndex() != 0) {
+                String sentencia
+                        = "SELECT nombre"
+                        + " FROM municipios JOIN provincias USING (id_provincia)"
+                        + " WHERE provincia = '" + provin + "'";
+
+                try (ResultSet rsMunicipios = s.executeQuery(sentencia)) {
+                    while (rsMunicipios.next()) {
+                        lm.addElement(rsMunicipios.getString("nombre"));
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage());
+        }
+    }
 
     public Interfaz() {
         initComponents();
@@ -53,6 +84,9 @@ public class Interfaz extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         municipios = new javax.swing.JList();
         salir = new javax.swing.JButton();
+        jButtonInsertarMunicipio = new javax.swing.JButton();
+        jTextNuevoMunicipio = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -82,29 +116,43 @@ public class Interfaz extends javax.swing.JFrame {
             }
         });
 
+        jButtonInsertarMunicipio.setText("Insertar");
+        jButtonInsertarMunicipio.setName("jButtonInsertarMunicipio"); // NOI18N
+        jButtonInsertarMunicipio.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonInsertarMunicipioMouseClicked(evt);
+            }
+        });
+
+        jTextNuevoMunicipio.setName("jTextNuevoMunicipio"); // NOI18N
+
+        jLabel1.setText("Nuevo Municipio:");
+        jLabel1.setName("jLabelNuevoMunicipio"); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(0, 0, Short.MAX_VALUE))))
+                        .addComponent(jLabel1)
+                        .addGap(7, 7, 7)
+                        .addComponent(jTextNuevoMunicipio)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonInsertarMunicipio))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(162, 162, 162)
-                                .addComponent(salir))
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(provincias, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                            .addComponent(jLabel2)
+                            .addComponent(provincias, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 110, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(162, 162, 162)
+                .addComponent(salir)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -116,8 +164,13 @@ public class Interfaz extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonInsertarMunicipio)
+                    .addComponent(jTextNuevoMunicipio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
                 .addComponent(salir)
-                .addContainerGap(27, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -148,27 +201,36 @@ public class Interfaz extends javax.swing.JFrame {
     }//GEN-LAST:event_formWindowOpened
 
     private void provinciasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_provinciasActionPerformed
-        lm.clear();
-        String provin = String.valueOf(provincias.getSelectedItem());
+        actualizarListaMunicipios();
+    }//GEN-LAST:event_provinciasActionPerformed
+
+    private void jButtonInsertarMunicipioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonInsertarMunicipioMouseClicked
+        String provincia = String.valueOf(provincias.getSelectedItem());
+        int _id_provincia = 0;
 
         try {  //   Mostramos los municipios de la provincia seleccionada
             Statement s = conexion.createStatement();
             if (provincias.getSelectedIndex() != 0) {
                 String sentencia
-                        = "SELECT nombre"
-                        + " FROM municipios JOIN provincias USING (id_provincia)"
-                        + " WHERE provincia = '" + provin + "'";
+                        = "SELECT id_provincia"
+                        + " FROM provincias"
+                        + " WHERE provincia = '" + provincia + "'";
 
-                try (ResultSet rsMunicipios = s.executeQuery(sentencia)) {
-                    while (rsMunicipios.next()) {
-                        lm.addElement(rsMunicipios.getString("nombre"));
+                try (ResultSet rsProvincias = s.executeQuery(sentencia)) {
+                    while (rsProvincias.next()) {
+                        _id_provincia = rsProvincias.getInt("id_provincia");
+                    }
+                    if(_id_provincia > 0) {
+                        insertarCategoria(jTextNuevoMunicipio.getText(), _id_provincia);
                     }
                 }
             }
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(this, ex.getMessage());
         }
-    }//GEN-LAST:event_provinciasActionPerformed
+        actualizarListaMunicipios();
+
+    }//GEN-LAST:event_jButtonInsertarMunicipioMouseClicked
 
     /**
      * @param args the command line arguments
@@ -206,8 +268,11 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonInsertarMunicipio;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextNuevoMunicipio;
     private javax.swing.JList municipios;
     private javax.swing.JComboBox provincias;
     private javax.swing.JButton salir;
