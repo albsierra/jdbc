3baed3b9ff6b28fbcd5afbeebc73fa21f93a7cfc
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-05-21 08:03:26 +0200

    Creando los controladores

---
+++ videoclubJPA/src/videoclubjpa/controllers/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package videoclubjpa.controllers.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
