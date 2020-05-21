8c2b321e1e993ffff0c12ca41cc253101a258012
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-05-21 08:32:36 +0200

    Probando a insertar a través del controlador

--- videoclubJPA/src/videoclubjpa/VideoclubJPA.java
+++ videoclubJPA/src/videoclubjpa/VideoclubJPA.java
@@ -5,7 +5,10 @@
  */
 package videoclubjpa;
 
+import javax.persistence.Persistence;
 import javax.swing.JFrame;
+import videoclubjpa.controllers.PeliculasJpaController;
+import videoclubjpa.entities.Peliculas;
 import videoclubjpa.views.PeliculasView;
 
 /**
@@ -18,11 +21,27 @@ public class VideoclubJPA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        insertaPelicula();
         JFrame peliculasFrame = new JFrame();
         peliculasFrame.setContentPane(new PeliculasView());
         peliculasFrame.setBounds(100, 100, 640, 480);
         peliculasFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         peliculasFrame.setVisible(true);
+    }
+
+    private static void insertaPelicula() {
+        PeliculasJpaController peliController = new PeliculasJpaController(
+                Persistence.createEntityManagerFactory("videoclubJPAPU")
+        );
+        peliController.create(
+                new Peliculas(
+                        null,
+                        "Probando el controlador",
+                        "Programación",
+                        60,
+                        "Alberto Sierra"
+                )
+        );
     }
     
 }
