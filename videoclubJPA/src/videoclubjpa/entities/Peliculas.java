2b145f590b3ad3302cefb25fa254deb19839345c
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-05-21 07:34:53 +0200

    Añadiendo las entidades

---
+++ videoclubJPA/src/videoclubjpa/entities/Peliculas.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoclubjpa.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author alumno
+ */
+@Entity
+@Table(name = "peliculas")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Peliculas.findAll", query = "SELECT p FROM Peliculas p")
+    , @NamedQuery(name = "Peliculas.findById", query = "SELECT p FROM Peliculas p WHERE p.id = :id")
+    , @NamedQuery(name = "Peliculas.findByTitulo", query = "SELECT p FROM Peliculas p WHERE p.titulo = :titulo")
+    , @NamedQuery(name = "Peliculas.findByGenero", query = "SELECT p FROM Peliculas p WHERE p.genero = :genero")
+    , @NamedQuery(name = "Peliculas.findByDuracion", query = "SELECT p FROM Peliculas p WHERE p.duracion = :duracion")
+    , @NamedQuery(name = "Peliculas.findByDirector", query = "SELECT p FROM Peliculas p WHERE p.director = :director")})
+public class Peliculas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "titulo")
+    private String titulo;
+    @Basic(optional = false)
+    @Column(name = "genero")
+    private String genero;
+    @Basic(optional = false)
+    @Column(name = "duracion")
+    private int duracion;
+    @Basic(optional = false)
+    @Column(name = "director")
+    private String director;
+
+    public Peliculas() {
+    }
+
+    public Peliculas(Integer id) {
+        this.id = id;
+    }
+
+    public Peliculas(Integer id, String titulo, String genero, int duracion, String director) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.director = director;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Peliculas)) {
+            return false;
+        }
+        Peliculas other = (Peliculas) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "videoclubjpa.entities.Peliculas[ id=" + id + " ]";
+    }
+    
+}
